5350a529d6ac16211edeaa474ebc1cc679d20806
ZYUIN1996 <133088892+ZYUIN1996@users.noreply.github.com>
2024-01-25 22:22:43 +0900

    initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,26 @@
-//TIP コードを<b>実行</b>するには、<shortcut actionId="Run"/> を押すか、
-// ガターで <icon src="AllIcons.Actions.Execute"/> アイコンをクリックします。
+import java.util.HashMap;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP ハイライトされたテキストにキャレットがある状態で <shortcut actionId="ShowIntentionActions"/> を押して
-        // IntelliJ IDEA がどのように修正を提案するか確認します。
-        System.out.printf("Hello and welcome!");
+        Map<String, List<String>> hamsterMap = new HashMap<>();
+        hamsterMap.put("ゴールデンハムスター", new ArrayList<String>());
+        hamsterMap.put("ロボロフスキーハムスター", new ArrayList<String>());
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP <shortcut actionId="Debug"/> を押してコードのデバッグを開始します。<icon src="AllIcons.Debugger.Db_set_breakpoint"/> ブレークポイントを 1 つ設定しました
-            // <shortcut actionId="ToggleLineBreakpoint"/> を押すといつでも他のブレークポイントを追加できます。
-            System.out.println("i = " + i);
+        hamsterMap.get("ゴールデンハムスター").add("ハム太郎");
+        hamsterMap.get("ロボロフスキーハムスター").add("ロボくん");
+
+        try {
+            for (Map.Entry<String, List<String>> entry : hamsterMap.entrySet()) {
+                System.out.println(entry.getKey() + "の名前:");
+                for (String name : entry.getValue()) {
+                    System.out.println(" - " + name);
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 }
